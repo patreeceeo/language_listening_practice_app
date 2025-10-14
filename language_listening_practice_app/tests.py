87d91bf1b3f6f85832a29eaea5aa8ae7eda45dcd
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2025-10-14 15:57:35 -0700

    DRY tests

--- language_listening_practice_app/tests.py
+++ language_listening_practice_app/tests.py
@@ -15,49 +15,42 @@ class ExercisePracticeSetTestCase(TestCase):
         self.next_user_id += 1
         return User.objects.create_user(username=f'user{id}', password='12345')
 
+    def _create_exercise(self, clip=None, type='shadow', question='', answers=None, correct_answer=''):
+        if clip is None:
+            clip = self._create_youtube_clip()
+        return Exercise.objects.create(
+            type=type,
+            youtube_clip=clip,
+            question=question,
+            answers=answers,
+            correct_answer=correct_answer
+        )
+
+    def _create_attempt(self, exercise, user, user_answer='', is_correct=False):
+        return ExerciseAttempt.objects.create(
+            user=user,
+            exercise=exercise,
+            user_answer=user_answer,
+            is_correct=is_correct
+        )
+
     def test_never_attempted(self):
         user = self._create_user()
-        Exercise.objects.create(
-            type='shadow',
-            youtube_clip=self._create_youtube_clip(),
-            question='',
-            correct_answer=''
-        )
+        self._create_exercise()
         exercises = Exercise.get_practice_set(user)
         self.assertEqual(exercises.count(), 1, "Should include never attempted exercises")
 
     def test_last_attempt_incorrect(self):
-        exercise = Exercise.objects.create(
-            type='transcribe',
-            youtube_clip=self._create_youtube_clip(),
-            question='What is said?',
-            correct_answer='Hello'
-        )
         user = self._create_user()
-        ExerciseAttempt.objects.create(
-            user=user,
-            exercise=exercise,
-            user_answer='Hi',
-            is_correct=False,
-        )
+        exercise = self._create_exercise()
+        self._create_attempt(exercise, user, user_answer='Hi', is_correct=False)
         exercises = Exercise.get_practice_set(user)
         self.assertIn(exercise, exercises, "Should include exercises with last attempt incorrect")
 
     def test_last_attempt_before_rest_interval(self):
-        exercise = Exercise.objects.create(
-            type='multiple_choice',
-            youtube_clip=self._create_youtube_clip(),
-            question='Choose the correct option',
-            answers=['Option A', 'Option B', 'Option C'],
-            correct_answer='Option A'
-        )
         user = self._create_user()
-        attempt = ExerciseAttempt.objects.create(
-            user=user,
-            exercise=exercise,
-            user_answer='Option A',
-            is_correct=True,
-        )
+        exercise = self._create_exercise()
+        attempt = self._create_attempt(exercise, user, user_answer='Option A', is_correct=True)
         # Have to manually set timestamp after creation because auto_now_add
         attempt.timestamp = relative_datetime(minutes=-10)
         attempt.save()
@@ -66,66 +59,30 @@ class ExercisePracticeSetTestCase(TestCase):
         self.assertIn(exercise, exercises, "Should include exercises last attempted before rest interval regardless of correctness")
 
     def test_attempted_by_other_user(self):
-        exercise = Exercise.objects.create(
-            type='shadow',
-            youtube_clip=self._create_youtube_clip(),
-            question='',
-            correct_answer=''
-        )
         user = self._create_user()
         other_user = self._create_user()
-        ExerciseAttempt.objects.create(
-            user=other_user,
-            exercise=exercise,
-            user_answer='',
-            is_correct=True,
-        )
+        exercise = self._create_exercise()
+        self._create_attempt(exercise, other_user, is_correct=True)
         exercises = Exercise.get_practice_set(user, relative_datetime(minutes=-1))
         self.assertIn(exercise, exercises, "Should include exercises attempted by other users")
 
     def test_multiple_attempts(self):
-        clip = self._create_youtube_clip()
-        exercise = Exercise.objects.create(
-            type='shadow',
-            youtube_clip=clip,
-            question='',
-            correct_answer=''
-        )
         user = self._create_user()
-        ExerciseAttempt.objects.create(
-            user=user,
-            exercise=exercise,
-            user_answer='',
-            is_correct=False,
-        )
-        ExerciseAttempt.objects.create(
-            user=user,
-            exercise=exercise,
-            user_answer='Hi',
-            is_correct=False,
-        )
+        exercise = self._create_exercise()
+        self._create_attempt(exercise, user, is_correct=False)
+        self._create_attempt(exercise, user, user_answer='Hi', is_correct=False)
         exercises = Exercise.get_practice_set(user, relative_datetime(minutes=-1))
         self.assertEqual(exercises.count(), 1, "Should return distinct exercises")
 
     def test_distinct_exercises(self):
         user = self._create_user()
-        ex1 = Exercise.objects.create(
-            type='shadow',
-            youtube_clip=self._create_youtube_clip(),
-            question='',
-            correct_answer=''
-        )
-        ex2 = Exercise.objects.create(
-            type='transcribe',
-            youtube_clip=self._create_youtube_clip(),
-            question='What is said?',
-            correct_answer='Hello'
-        )
+        ex1 = self._create_exercise()
+        ex2 = self._create_exercise()
 
         # Create multiple attempts for each
-        ExerciseAttempt.objects.create(user=user, exercise=ex1, is_correct=False)
-        ExerciseAttempt.objects.create(user=user, exercise=ex1, is_correct=False)
-        ExerciseAttempt.objects.create(user=user, exercise=ex2, is_correct=False)
+        self._create_attempt(ex1, user, is_correct=False)
+        self._create_attempt(ex1, user, is_correct=False)
+        self._create_attempt(ex2, user, is_correct=False)
 
         exercises = Exercise.get_practice_set(user)
         exercise_list = list(exercises)
@@ -144,17 +101,10 @@ class ExercisePracticeSetTestCase(TestCase):
 
     def test_distinct_when_multiple_conditions_match(self):
         user = self._create_user()
-        exercise = Exercise.objects.create(
-            type='shadow',
-            youtube_clip=self._create_youtube_clip(),
-            question='',
-            correct_answer=''
-        )
+        exercise = self._create_exercise()
 
         # Create attempt that's BOTH incorrect AND old
-        attempt = ExerciseAttempt.objects.create(
-                user=user, exercise=exercise, is_correct=False
-                )
+        attempt = self._create_attempt(user=user, exercise=exercise, is_correct=False)
         attempt.timestamp = relative_datetime(minutes=-10)
         attempt.save()
 
